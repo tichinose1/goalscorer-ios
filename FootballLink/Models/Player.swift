0feae8b4c67e5513f772f3002f723044c852e71c
tichinose1 <toyoh.ichinose@unisys.co.jp>
2018-11-11 19:27:50 +0900

    データをModelにまとめるように修正した #1

--- FootballLink/Models/Player.swift
+++ FootballLink/Models/Player.swift
@@ -11,3 +11,37 @@ struct Player {
     let regionCode: String
     let url: String
 }
+
+extension Player {
+
+    static let all = [
+        Player(name: "Lionel Messi", regionCode: "AR", url: "https://en.wikipedia.org/wiki/Lionel_Messi#Career_statistics"),
+        Player(name: "Cristiano Ronaldo", regionCode: "PT", url: "https://en.wikipedia.org/wiki/Cristiano_Ronaldo#Career_statistics"),
+        Player(name: "Luis Suárez", regionCode: "UY", url: "https://en.wikipedia.org/wiki/Luis_Su%C3%A1rez#Career_statistics"),
+        Player(name: "Robert Lewandowski", regionCode: "PL", url: "https://en.wikipedia.org/wiki/Robert_Lewandowski#Career_statistics"),
+        Player(name: "Sergio Agüero", regionCode: "AR", url: "https://en.wikipedia.org/wiki/Sergio_Ag%C3%BCero#Career_statistics"),
+        Player(name: "Neymar", regionCode: "BR", url: "https://en.wikipedia.org/wiki/Neymar#Career_statistics"),
+        Player(name: "Gonzalo Higuaín", regionCode: "AR", url: "https://en.wikipedia.org/wiki/Gonzalo_Higua%C3%ADn#Career_statistics"),
+        Player(name: "Zlatan Ibrahimović", regionCode: "SE", url: "https://en.wikipedia.org/wiki/Zlatan_Ibrahimovi%C4%87#Career_statistics"),
+        Player(name: "Wayne Rooney", regionCode: "GB-ENG", url: "https://en.wikipedia.org/wiki/Wayne_Rooney#Career_statistics"),
+        Player(name: "David Villa", regionCode: "ES", url: "https://en.wikipedia.org/wiki/David_Villa#Career_statistics"),
+        Player(name: "Samuel Eto'o", regionCode: "CM", url: "https://en.wikipedia.org/wiki/Samuel_Eto%27o#Career_statistics"),
+        Player(name: "Robbie Keane", regionCode: "IE", url: "https://en.wikipedia.org/wiki/Robbie_Keane#Career_statistics"),
+        Player(name: "Robin van Persie", regionCode: "NL", url: "https://en.wikipedia.org/wiki/Robin_van_Persie#Career_statistics"),
+        Player(name: "Klaas-Jan Huntelaar", regionCode: "NL", url: "https://en.wikipedia.org/wiki/Klaas-Jan_Huntelaar#Career_statistics"),
+        Player(name: "Carlos Tevez", regionCode: "AR", url: "https://en.wikipedia.org/wiki/Carlos_Tevez#Career_statistics"),
+        Player(name: "Mauro Icardi", regionCode: "AR", url: "https://en.wikipedia.org/wiki/Mauro_Icardi#Career_statistics"),
+        Player(name: "Edinson Cavani", regionCode: "UY", url: "https://en.wikipedia.org/wiki/Edinson_Cavani#Career_statistics"),
+        Player(name: "Edin Džeko", regionCode: "BA", url: "https://en.wikipedia.org/wiki/Edin_D%C5%BEeko#Career_statistics"),
+        Player(name: "Harry Kane", regionCode: "GB-ENG", url: "https://en.wikipedia.org/wiki/Harry_Kane#Career_statistics"),
+        Player(name: "Karim Benzema", regionCode: "FR", url: "https://en.wikipedia.org/wiki/Karim_Benzema#Career_statistics"),
+        Player(name: "Antoine Griezmann", regionCode: "FR", url: "https://en.wikipedia.org/wiki/Antoine_Griezmann#Career_statistics"),
+        Player(name: "Kylian Mbappé", regionCode: "FR", url: "https://en.wikipedia.org/wiki/Kylian_Mbapp%C3%A9#Career_statistics"),
+        Player(name: "Paulo Dybala", regionCode: "AR", url: "https://en.wikipedia.org/wiki/Paulo_Dybala#Career_statistics")
+    ]
+
+    static let teemplates = [
+        Player(name: "", regionCode: "", url: ""),
+        Player(name: "", regionCode: "", url: "")
+    ]
+}
--- FootballLink/Models/TopScorer.swift
+++ FootballLink/Models/TopScorer.swift
@@ -11,3 +11,32 @@ struct TopScorer {
     let regionCode: String
     let url: String
 }
+
+extension TopScorer {
+
+    static let all = [
+        TopScorer(title: "2018–19 UEFA Champions League", regionCode:"EU", url: "https://en.wikipedia.org/wiki/2018%E2%80%9319_UEFA_Champions_League#Top_goalscorers"),
+        TopScorer(title: "2018–19 UEFA Europa League", regionCode:"EU", url: "https://en.wikipedia.org/wiki/2018%E2%80%9319_UEFA_Europa_League#Top_goalscorers"),
+        TopScorer(title: "2018–19 La Liga", regionCode:"ES", url: "https://en.wikipedia.org/wiki/2018%E2%80%9319_La_Liga#Top_goalscorers"),
+        TopScorer(title: "2018–19 Premier League", regionCode:"GB-ENG", url: "https://en.wikipedia.org/wiki/2018%E2%80%9319_Premier_League#Top_scorers"),
+        TopScorer(title: "2018–19 Serie A", regionCode:"IT", url: "https://en.wikipedia.org/wiki/2018%E2%80%9319_Serie_A#Top_goalscorers"),
+        TopScorer(title: "2018–19 Bundesliga", regionCode:"DE", url: "https://en.wikipedia.org/wiki/2018%E2%80%9319_Bundesliga#Top_scorers"),
+        TopScorer(title: "2018–19 Ligue 1", regionCode:"FR", url: "https://en.wikipedia.org/wiki/2018%E2%80%9319_Ligue_1#Top_goalscorers"),
+        TopScorer(title: "2018–19 Primeira Liga", regionCode:"PT", url: "https://en.wikipedia.org/wiki/2018%E2%80%9319_Primeira_Liga#Season_summary"),
+        TopScorer(title: "2018–19 Eredivisie", regionCode:"NL", url: "https://en.wikipedia.org/wiki/2018%E2%80%9319_Eredivisie#Top_scorers"),
+        TopScorer(title: "2018–19 Belgian First Division A", regionCode:"BE", url: "https://en.wikipedia.org/wiki/2018%E2%80%9319_Belgian_First_Division_A#Top_scorers"),
+        TopScorer(title: "2018–19 Austrian Football Bundesliga", regionCode:"AT", url: "https://en.wikipedia.org/wiki/2018%E2%80%9319_Austrian_Football_Bundesliga#Top_scorers"),
+        TopScorer(title: "2018 Campeonato Brasileiro Série A", regionCode:"BR", url: "https://en.wikipedia.org/wiki/2018_Campeonato_Brasileiro_S%C3%A9rie_A#Top_scorers"),
+        TopScorer(title: "2018–19 Argentine Primera División", regionCode:"AR", url: "https://en.wikipedia.org/wiki/2018%E2%80%9319_Argentine_Primera_Divisi%C3%B3n#Top_goalscorers"),
+        TopScorer(title: "2018 Major League Soccer season", regionCode:"US", url: "https://en.wikipedia.org/wiki/2018_Major_League_Soccer_season#Goals"),
+        TopScorer(title: "2018–19 Liga MX season", regionCode:"MX", url: "https://en.wikipedia.org/wiki/2018%E2%80%9319_Liga_MX_season#Top_goalscorers"),
+        TopScorer(title: "2018 Chinese Super League", regionCode:"CN", url: "https://en.wikipedia.org/wiki/2018_Chinese_Super_League#Top_scorers"),
+        TopScorer(title: "2018 K League 1", regionCode:"KR", url: "https://en.wikipedia.org/wiki/2018_K_League_1#Top_scorers"),
+        TopScorer(title: "2018 J1 League", regionCode:"JP", url: "https://en.wikipedia.org/wiki/2018_J1_League#Top_scorers")
+    ]
+
+    static let teemplates = [
+        TopScorer(title: "", regionCode:"", url: ""),
+        TopScorer(title: "", regionCode:"", url: "")
+    ]
+}
