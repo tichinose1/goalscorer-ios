8de9d961bad5d74ec393af2bad7982121ca5a476
tichinose1 <toyoh.ichinose@unisys.co.jp>
2018-12-28 14:12:26 +0900

    Add global error handlers

--- Goalscorer/AppDelegate.swift
+++ Goalscorer/AppDelegate.swift
@@ -14,6 +14,10 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     var window: UIWindow?
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        NSSetUncaughtExceptionHandler {
+            print($0)
+        }
+
         return true
     }
 }
