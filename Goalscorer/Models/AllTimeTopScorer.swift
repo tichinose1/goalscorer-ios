09ce9327ea099f2903e93f926edd75f6599b1298
tichinose1 <tichinose1dev@gmail.com>
2019-01-02 03:09:55 +0900

    Add several all-time top scorers

--- Goalscorer/Models/AllTimeTopScorer.swift
+++ Goalscorer/Models/AllTimeTopScorer.swift
@@ -15,14 +15,22 @@ extension AllTimeTopScorer {
 
     // swiftlint:disable all
     static let all = [
+        // National Team
         AllTimeTopScorer(competition: .fifaWorldCup, url: "https://en.wikipedia.org/wiki/FIFA_World_Cup_top_goalscorers#Overall_top_goalscorers"),
-        AllTimeTopScorer(competition: .copaAmerica, url: "https://en.wikipedia.org/wiki/Copa_Am%C3%A9rica_records_and_statistics#Goalscoring"),
         AllTimeTopScorer(competition: .uefaEuro, url: "https://en.wikipedia.org/wiki/List_of_UEFA_European_Championship_goalscorers#Overall_top_goalscorers"),
+        AllTimeTopScorer(competition: .copaAmerica, url: "https://en.wikipedia.org/wiki/Copa_Am%C3%A9rica_records_and_statistics#Goalscoring"),
+        // Club
+        AllTimeTopScorer(competition: .fifaClubWorldCup, url: "https://en.wikipedia.org/wiki/List_of_FIFA_Club_World_Cup_goalscorers#Overall_top_goalscorers"),
         AllTimeTopScorer(competition: .uefaChampionsLeague, url: "https://en.wikipedia.org/wiki/List_of_European_Cup_and_UEFA_Champions_League_top_scorers#All-time_top_scorers"),
+        AllTimeTopScorer(competition: .copaLibertadores, url: "https://en.wikipedia.org/wiki/Copa_Libertadores_records_and_statistics#All-time_top_scorers"),
+        AllTimeTopScorer(competition: .concacafChampionsLeague, url: "https://en.wikipedia.org/wiki/CONCACAF_Champions%27_Cup_and_Champions_League_records_and_statistics#All-time_top_scorers"),
+        AllTimeTopScorer(competition: .afcChampionsLeague, url: "https://en.wikipedia.org/wiki/Asian_Club_Championship_and_AFC_Champions_League_records_and_statistics#All-time_top_goalscorers"),
+        // League
         AllTimeTopScorer(competition: .laLiga, url: "https://en.wikipedia.org/wiki/List_of_La_Liga_top_scorers#All-time_top_scorers"),
         AllTimeTopScorer(competition: .premierLeague, url: "https://en.wikipedia.org/wiki/List_of_Premier_League_players_with_100_or_more_goals#Players"),
         AllTimeTopScorer(competition: .serieA, url: "https://en.wikipedia.org/wiki/List_of_Serie_A_players_with_100_or_more_goals#List"),
         AllTimeTopScorer(competition: .bundesliga, url: "https://en.wikipedia.org/wiki/Bundesliga#Top_scorers"),
+        AllTimeTopScorer(competition: .ligue1, url: "https://en.wikipedia.org/wiki/List_of_Ligue_1_records_and_statistics#Goalscorers"),
         AllTimeTopScorer(competition: .campeonatoBrasileiroSerieA, url: "https://en.wikipedia.org/wiki/Campeonato_Brasileiro_S%C3%A9rie_A_top_scorers#All-time_top_scorers"),
         AllTimeTopScorer(competition: .argentinePrimeraDivision, url: "https://en.wikipedia.org/wiki/List_of_Argentine_Primera_Divisi%C3%B3n_top_scorers#All-time_Topscorers"),
         AllTimeTopScorer(competition: .j1League, url: "https://en.wikipedia.org/wiki/J.League_records_and_statistics#Ranks")
