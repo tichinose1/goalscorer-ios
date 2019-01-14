7a0e9b60a5f18d7d167d0f142de6cb2746ef8a9c
tichinose1 <tichinose1dev@gmail.com>
2019-01-15 03:33:56 +0900

    Remove fabric_api_key from info.plist

--- Goalscorer/AppDelegate.swift
+++ Goalscorer/AppDelegate.swift
@@ -21,7 +21,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     var window: UIWindow?
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
-        Fabric.with([Crashlytics.self])
+        Crashlytics.start(withAPIKey: "fabric_api_key")
 
         NSSetUncaughtExceptionHandler {
             print($0)
